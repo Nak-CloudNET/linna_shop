a2f792a68d0f8228aa5599ac1e7a9a9f6f632792
Nak <khat.ratanak@cloudnet.com.kh>
2018-03-31 09:17:08 +0700

    Nak Upload System

---
+++ themes/default/views/sales/invoice_a5.php
@@ -0,0 +1,623 @@
+<!Doctype>
+<html>
+      <head>
+	         <style>
+			          @media print{
+					     .table{
+						       width:100%!important;  
+							   margin:0px auto;
+					    }
+						.table-foot{
+							width:100% !important;
+						}
+						.box-left{
+							border: 1px solid black;
+							float: left;
+							height: 86px;
+							width: 90%;
+							margin-left:2%; !important;
+					    }
+						.box-right{
+							border: 1px solid black;
+							float: right;
+							height: 86px;
+							width: 90%;  
+						}
+						 .table-item{
+								padding-left:1.5%;
+								padding-right:1.5%;
+						}
+						#table{
+							width:100% !important;
+						}
+
+					 }
+			        .table{
+						width:80%;
+						margin:0px auto;
+					}
+					.box-left{
+						border: 1px solid black;
+						float: left;
+						height: 86px;
+						width: 90%;
+						padding-left:5px;
+					}
+					 
+					.box-right{
+						border: 1px solid black;
+						float: left;
+						height: 60px;
+						width: 100%;
+						padding-left:5px;
+					}
+					.bg1{
+						width:63%;
+						float:left;
+					}
+					.bg{
+						width:35%;
+						float:left;
+					}
+			        #paragraph p{
+						line-height:10px;
+					}
+					
+			 </style>
+	  
+	  </head>
+	  <body>
+	          <table class="table"> 
+					         <tr>
+							      <td colspan="2"style="text-align:center"><h1><?=lang("Tea_tree_II")?></h1></td>
+							 </tr>
+					         <tr>
+							    <td colspan='2'>
+							    <table width="100%">
+								   <tr>
+									  <td class="bg1">
+										   <table class="box-left">
+												   <tr>
+													   <td width="15%"><?=lang("customer")?></td>
+													   <td>:</td>
+												   </tr>
+												   <tr >
+													   <td><?=lang("address")?></td>
+													   <td>:</td>
+												   </tr>
+												   <tr >
+													   <td><?=lang("Tel/Fax")?></td>
+													   <td>:</td>
+												   </tr>
+										   </table> 				   
+									  </td>
+									  <td class="bg">
+										   <table class="box-right">
+												   <tr>
+													   <td width="25%"><?=lang("date")?></td>
+													   <td>:</td>
+												   </tr>
+												   <tr>
+													   <td><?=lang("invoice_no")?></td>
+													   <td>:</td>
+												   </tr>
+												  
+										   </table> 
+									  </td>
+									</tr>
+								  </table>
+								  </td>
+							 </tr> 
+					  <tbody>
+					         <tr>
+							      <td class="table-item" colspan="2">
+								         <table id="table"  width="99%" border="1px" style="border-collapse: collapse";>
+										    <thead>
+										         <tr>
+												     <th ><?=lang("ល.រ<br>No")?></th>
+													 <th><?=lang("លេខកូដទំនិញ<br>Product Code")?></th>
+													 <th><?=lang("ឈ្មោះទំនិញ<br>Description")?></th>
+													 <th><?=lang("ខ្នាត<br>Unit")?></th>
+                                                     <th><?=lang("ចំនួន<br>Qty")?></th>
+													 <th><?=lang("តំលៃ<br>Unit Price")?></th>
+													 <th><?=lang("សរុប<br>Amount")?></th>
+													  													 
+												 </tr>
+											</thead>
+											<tbody>
+												 <tr>
+												     <td>&nbsp;</td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+												 </tr>
+												  <tr>
+												     <td>&nbsp;</td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+												 </tr>
+												 <tr>
+												     <td>&nbsp;</td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+												 </tr>
+												  <tr>
+												     <td>&nbsp;</td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+												 </tr>
+												 <tr>
+												     <td>&nbsp;</td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+												 </tr>
+												  <tr>
+												     <td>&nbsp;</td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+												 </tr>
+												 <tr>
+												     <td>&nbsp;</td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+												 </tr>
+												  <tr>
+												     <td>&nbsp;</td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+												 </tr>
+												 <tr>
+												     <td>&nbsp;</td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+												 </tr>
+												  <tr>
+												     <td>&nbsp;</td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+												 </tr>
+												 <tr>
+												     <td>&nbsp;</td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+												 </tr>
+												  <tr>
+												     <td>&nbsp;</td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+												 </tr>
+												 <tr>
+												     <td>&nbsp;</td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+												 </tr>
+												  <tr>
+												     <td>&nbsp;</td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+												 </tr>
+												 <tr>
+												     <td>&nbsp;</td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+												 </tr>
+												  <tr>
+												     <td>&nbsp;</td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+												 </tr>
+												 <tr>
+												     <td>&nbsp;</td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+												 </tr>
+												  <tr>
+												     <td>&nbsp;</td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+												 </tr>
+												 <tr>
+												     <td>&nbsp;</td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+												 </tr>
+												  <tr>
+												     <td>&nbsp;</td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+												 </tr>
+												 <tr>
+												     <td>&nbsp;</td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+												 </tr>
+												  <tr>
+												     <td>&nbsp;</td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+												 </tr>
+												 <tr>
+												     <td>&nbsp;</td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+												 </tr>
+												  <tr>
+												     <td>&nbsp;</td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+												 </tr>
+												 <tr>
+												     <td>&nbsp;</td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+												 </tr>
+												  <tr>
+												     <td>&nbsp;</td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+												 </tr>
+												 <tr>
+												     <td>&nbsp;</td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+												 </tr>
+												  <tr>
+												     <td>&nbsp;</td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+												 </tr>
+												 <tr>
+												     <td>&nbsp;</td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+												 </tr>
+												  <tr>
+												     <td>&nbsp;</td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+												 </tr>
+												  <tr>
+												     <td>&nbsp;</td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+												 </tr>
+												 <tr>
+												     <td>&nbsp;</td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+												 </tr>
+												  <tr>
+												     <td>&nbsp;</td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+												 </tr>
+												  <tr>
+												     <td>&nbsp;</td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+												 </tr>
+												 <tr>
+												     <td>&nbsp;</td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+												 </tr>
+												  <tr>
+												     <td>&nbsp;</td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+												 </tr>
+												  <tr>
+												     <td>&nbsp;</td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+												 </tr>
+												 <tr>
+												     <td>&nbsp;</td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+												 </tr>
+												  <tr>
+												     <td>&nbsp;</td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+												 </tr>
+												  <tr>
+												     <td>&nbsp;</td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+												 </tr>
+												 <tr>
+												     <td>&nbsp;</td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+												 </tr>
+												  <tr>
+												     <td>&nbsp;</td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+												 </tr>
+												  <tr>
+												     <td>&nbsp;</td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+												 </tr>
+												 <tr>
+												     <td>&nbsp;</td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+												 </tr>
+												  <tr>
+												     <td>&nbsp;</td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+												 </tr>
+												  <tr>
+												     <td>&nbsp;</td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+												 </tr>
+												 <tr>
+												     <td>&nbsp;</td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+												 </tr>
+												  <tr>
+												     <td>&nbsp;</td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+												 </tr>
+                                                 <tr>
+												     <td>&nbsp;</td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+												 </tr>
+												  <tr>
+												     <td>&nbsp;</td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+													 <td></td>
+												 </tr>
+										  </tbody>
+										 </table>
+								  </td>
+							 </tr>
+					  </tbody>
+					 
+			  </table>
+			  <table class="table-foot"style="page-break-before: always;width:80%;margin:0px auto">
+			   <tfoot>
+					           <tr>
+							        <td>
+									       <h3>Additional Remark</h3> 
+										   <p>-ទំនិញទិញហើយមិនអាចប្តូរយកប្រាក់វិញបានឡើយ.</p>
+										   <p>-អ្នកទិញត្រូវរាប់និងពិនិត្យទំនិញឲបានត្រឺមត្រូវ មុនចុះហត្ថលេខាទទួល.</p>
+										   <p>-ទំនិញដែលប្រើសល់អាចសង់ចូលវិញបានតែក្នុងរយះពេល ៣០ថ្ងៃចាប់ ពីថ្ងៃទទួលទំនិញ.</p>
+										   <p>-ចំពោះទំនិញដែលបានកម្មង់គឺមិនអាចសង់ចូលវិញបានទេ.</p>
+										   <p>-តំលៃទំនិញទាំងអស់នេះប្រាក់ពន្ធបន្ថែម VAT 10%​ ជាបន្ទុករបស់នាក់ទិញ</p>
+										    								  
+									</td>
+									<td>
+									      <table>
+												 <tr>
+													  <td width="20%"><h4><?= lang("grand_total") ?>&nbsp;&nbsp;&nbsp;&nbsp;:</h2></td>
+													  <td style=""><p style="text-align:center;padding:0px;border-bottom:dotted 1px black;"><?=$this->erp->formatMoney($total)?>&nbsp;&nbsp; $</p></td>
+												 </tr>
+												 <tr>
+													  <td><h4><?= lang("paid") ?>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:</h4></td>
+													  <td style=""><p style="text-align:center;padding:0px;border-bottom:dotted 1px black;"><?=$this->erp->formatMoney($inv->paid)?>&nbsp;&nbsp; $</p></td>
+												 </tr>
+												 <tr>
+													  <td><h4><?= lang("balance") ?>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:</h4></td>
+													  <td style=""><p style="text-align:center;padding:0px;border-bottom:dotted 1px black;"><?=$this->erp->formatMoney($inv->grand_total-$inv->paid);?>&nbsp;&nbsp; $</td>
+												 </tr>
+												 <tr>
+													  <td><h4><?= lang("amount_in_word") ?></h4></td>
+													  <td></td>
+												 </tr>
+										  </table>
+										  
+									</td>
+									 
+							   </tr>
+					  </tfoot>
+		 </table>
+	  </body>
+</html>
